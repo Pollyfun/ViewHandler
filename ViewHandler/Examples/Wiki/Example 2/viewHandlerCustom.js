46cb66b5bcb0e2f1284ba1b44e6be367339da6b0
SVART\Joakim <illuvatargames@gmail.com>
2017-05-17 22:39:53 +0200

    more

---
+++ ViewHandler/Examples/Wiki/Example 2/viewHandlerCustom.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+function configureView(viewConfig) {
+   var configName = viewConfig.configName;
+
+   if (configName === 'Example2.config') {
+      viewConfig.addDataStore('/Examples/Wiki/Example 2/standard-data.json');
+
+      viewConfig.addColumns([
+           { title: 'Duration', sort: true },
+           { title: 'Duration-numSort', sort: true, numSort: true }
+      ]);
+   }
+}
