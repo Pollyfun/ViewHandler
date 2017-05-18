d3d1da1cb4b6bef45b36cb602631a1157f02530c
SVART\Joakim <illuvatargames@gmail.com>
2017-05-18 23:26:52 +0200

    Examples improved: demo-text, configuration, link to code

--- ViewHandler/Examples/Wiki/Example 8/viewHandlerCustom.js
+++ ViewHandler/Examples/Wiki/Example 8/viewHandlerCustom.js
@@ -5,8 +5,7 @@ function configureView(viewConfig) {
 
    if (configName === 'Example8.config') {
       viewConfig.addDataStore('standard-data.json');
-      //viewConfig.addCss('/Examples/Wiki/Example 8/Example.css');
-      viewConfig.addCss('Example.css');
+      viewConfig.addCss('Examples/Wiki/Example 8/Example.css');
 
       viewConfig.addColumns([
            { title: 'formating', type: COLUMN.CLASSES }
